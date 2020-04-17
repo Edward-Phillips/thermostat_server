4e598b63f5447a84b45eafe5ff405634c52d2a84
Edward Phillips <edd0ddi0@gmail.com>
2020-04-17 15:29:11 +0100

    unit test - thermostat should be able to update the temperature - failing

--- spec/thermostat_spec.rb
+++ spec/thermostat_spec.rb
@@ -4,4 +4,8 @@ describe Thermostat do
   it " should be able to store the temperature" do
     expect(subject).to respond_to(:temperature)
   end
+
+  it " should be able to update the temperature" do 
+    expect(subject).to respond_to(:temperature=) 
+  end
 end
